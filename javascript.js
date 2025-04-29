a602092a6ce5bc80f43626cd45c867eca7b46c07
thinguyen15 <ha.nguyen@hotmail.fi>
2025-04-29 21:32:53 +0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("mobileMenu");
-    
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
